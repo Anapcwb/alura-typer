97fa61697fffa833154a940da2fdc1c02aff38d7
Ana Paula <anapcwb@gmail.com>
2023-04-25 21:42:30 +0100

    added element linha using function jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -53,8 +53,6 @@ function inicializaMarcadores(){
     campo.on("input",function(){
         var digitado = campo.val();
         var comparavel = frase.substr(0, digitado.length);
-        console.log("Digitado: " + digitado);
-        console.log("Frase C.: " + comparavel);
 
         if(digitado == comparavel){
         campo.addClass("borda-verde");
@@ -70,21 +68,37 @@ function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Ana";
     var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
 
-    var linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                    "<td>"+ botaoRemover + "</td>"+
-                "</tr>";
+    var linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
     corpoTabela.prepend(linha);
 }
 
-$(".botao-remover").click(function(event){
-    event.preventDefault();
-    $(this).parent().parent().remove();
-});
+function novaLinha(usuario,palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
 
+    var link = $("<a>").addClass("botao-remover").attr("href","#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(){
+        event.preventDefault();
+        $(this).parent().parent().remove();
+}
 
 function reiniciaJogo(){
     campo.attr("disabled",false);
