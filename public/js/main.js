ba45d606d7d5b6b1a0e4a559bfff3536caf212bb
Ana Paula <anapcwb@gmail.com>
2023-04-25 13:32:16 +0100

    added botao reiniciar

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
+var tempoInicial = $("#tempo-digitacao").text();
+
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras)
-
 
 var campo = $(".campo-digitacao");
 campo.on("input",function(){
@@ -19,7 +20,6 @@ var tempoRestante = $("#tempo-digitacao").text();
 campo.one("focus",function(){
     var cronometroID = setInterval(function(){
         tempoRestante--;
-        console.log(tempoRestante);
         $("#tempo-digitacao").text(tempoRestante)
         if(tempoRestante < 1){
             campo.attr("disabled", true);
@@ -28,4 +28,12 @@ campo.one("focus",function(){
     },1000);   
 });
 
+$("#botao-reiniciar").click(function(){
+    campo.attr("disabled",false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
 
+    console.log("botao clicado");
+});
