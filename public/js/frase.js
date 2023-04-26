731f85931f623c8d535a977657277d91342b3b20
Ana Paula <anapcwb@gmail.com>
2023-04-26 20:20:44 +0100

    insert fail message and add spinner to change sentence

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,18 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+    $("#spinner").show();
+
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function(){
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").toggle();
+        }, 2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
         //A função fraseAleatoria será a responsável por fazer uma requisição ao nosso servidor (http://localhost:3000/frases) e pegar uma frase, para podermos modificar oHTML da nossa página com essa nova frase.
 }
 
