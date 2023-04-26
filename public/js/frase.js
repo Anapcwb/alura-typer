3672148ad770bf1028cbcc8b147b7e87061404e4
Ana Paula <anapcwb@gmail.com>
2023-04-26 13:39:09 +0100

    added functions fraseAleatoria e trocaFraseAleatoria em frase.js

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,5 +1,15 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-    console.log("Foi clicado");   
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+        //A função fraseAleatoria será a responsável por fazer uma requisição ao nosso servidor (http://localhost:3000/frases) e pegar uma frase, para podermos modificar oHTML da nossa página com essa nova frase.
 }
+
+function trocaFraseAleatoria(data){
+    var frase = $(".frase");
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
+
+    frase.text(data[numeroAleatorio].texto);
+}
+
+ 
