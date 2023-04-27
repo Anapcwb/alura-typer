c2dfd08bcebceeedaa1ab1c0e4fa95622b66665a
Ana Paula <anapcwb@gmail.com>
2023-04-27 10:09:12 +0100

    created function atualizaPlacar that get placar data and added to HTML

--- public/js/placar.js
+++ public/js/placar.js
@@ -83,3 +83,13 @@ function sincronizaPlacar(){
     console.log("salvou o placar");
    });
 }
+
+function atualizaPlacar(){
+
+   $.get("http://localhost:3000/placar",function(data){ 
+        $(data).each(function(){
+            var linha = novaLinha(this.usuario, this.pontos);
+            $("tbody").append(linha);
+        });  
+});
+}
